1c32afb4503f714115349ca91d9758d15062140a
whdgusdl48 <52066712+whdgusdl48@users.noreply.github.com>
2021-02-23 17:17:37 +0900

    JadenCase

---
+++ Level2/solution_2021_02_23.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = ''
+    for idx, i in enumerate(s): 
+        if (idx == 0): 
+            answer = i.upper()
+        elif s[idx-1] == ' ': 
+            answer = ''.join([answer, i.upper()])
+        else: 
+            answer = ''.join([answer, i.lower()])
+    return answer
