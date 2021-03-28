02d69ef8bf7ea1e0c6793fe08e4900784707e451
whdgusdl48 <52066712+whdgusdl48@users.noreply.github.com>
2021-03-28 18:41:58 +0900

    베스트 앨범

---
+++ Level3/solutuon_2021_03_28.py
@@ -0,0 +1,17 @@
+def solution(genres, plays):
+    answer = []
+    playsDict = {}
+    d = { }
+    for i in range(len(genres)):
+        genre = genres[i]
+        play = plays[i]
+        playsDict[genre] = playsDict.get(genre, 0) + play
+        d[genre] = d.get(genre, []) + [ (play, i) ]
+
+    genreSort = sorted(playsDict.items(), key=lambda x: x[1], reverse=True)
+    
+    for (genre, totalPlay) in genreSort:
+        d[genre] = sorted(d[genre], key=lambda x: (-x[0], x[1]))
+        answer += [ idx for (play, idx) in d[genre][:2] ]
+    
+    return answer
